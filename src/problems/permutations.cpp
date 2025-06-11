e1cb2ee50715b318e538ed95d16236ac58675c0c
Bogdan Floris <bogdan.floris@gmail.com>
2025-06-11 15:23:13 +0300

    feat: permutations

---
+++ src/problems/permutations.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+
+    if (n == 1) {
+        std::cout << "1\n";
+        return 0;
+    }
+
+    if (n <= 3) {
+        std::cout << "NO SOLUTION\n";
+        return 0;
+    }
+
+    for (int i = 2; i <= n; i += 2) {
+        std::cout << i << " ";
+    }
+    for (int i = 1; i <= n; i += 2) {
+        std::cout << i << " ";
+    }
+
+    std::cout << "\n";
+
+    return 0;
+}
